e1c8d7ef2f77f8c800f8204b16965bd22b009cfd
md-mudassir7 <mohammedmudassir.soudagar@amagi.com>
2024-04-20 17:32:25 +0530

    add: basic implementation

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/md-mudassir7/chill-party
+
+go 1.21.3
+
+require github.com/gorilla/mux v1.8.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/md-mudassir7/chill-party/server"
+)
+
+func main() {
+	server.Start()
+}
